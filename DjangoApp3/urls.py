d63777030e617c95c8a4eb18d6e072321b0eb239
Paweł Rudnicki <pawel.rudnicki@lama-media.com>
2022-06-25 14:21:50 +0200

    urls

--- DjangoApp3/urls.py
+++ DjangoApp3/urls.py
@@ -20,4 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
